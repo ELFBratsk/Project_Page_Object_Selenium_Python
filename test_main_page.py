a102c7f9f65bee2f4c508180ae66dde47813c099
ELFBratsk <vitaliy_bratsk@mail.ru>
2022-04-04 22:04:34 +0300

    Первый тест на основе Page Object / The first test based on Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,8 @@
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from more_itertools import time_limited
+from .pages.main_page import MainPage
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
     
-def test_guest_can_go_to_login_page(browser): 
-   browser.get(link) 
-   go_to_login_page(browser) 
-'''
-def test_add_to_cart(browser):
-    page = ProductPage(url="", browser)   # инициализируем объект Page Object
-    page.open()                           # открываем страницу в браузере
-    page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
-    page.add_product_to_cart()            # жмем кнопку добавить в корзину 
-    page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
-'''
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор      экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
