a8636fd2af5d2ffb9f741a798f3ce7949f90e9e7
ELFBratsk <vitaliy_bratsk@mail.ru>
2022-04-04 22:21:44 +0300

    Проверка элемента на странице / Checking an element on a page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,15 @@
 from more_itertools import time_limited
 from .pages.main_page import MainPage
+from selenium.webdriver.common.by import By
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор      экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
