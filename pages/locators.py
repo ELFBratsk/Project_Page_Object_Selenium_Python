aa87910f0327faf13e08b3a68373b18d2b32e630
Vitaliy <93813005+ELFBratsk@users.noreply.github.com>
2022-04-07 13:25:26 +0300

    Добавил locators.py / Added locators.py

---
+++ pages/locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+# Внутри создайте новый класс. Каждый класс будет соответствовать каждому классу PageObject: 
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link") # теперь каждый селектор — это пара: как искать и что искать.
+    #
+    
