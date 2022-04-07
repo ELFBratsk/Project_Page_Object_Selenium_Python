aba8c3c41fdede1f99f2c64f844b9f5ea865c510
ELFBratsk <vitaliy_bratsk@mail.ru>
2022-04-07 13:11:01 +0300

    Добавил locators.py и доработал Test / Added locators.py and modified the Test

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,17 @@
+from selenium.common.exceptions import NoSuchElementException # Чтобы импортировать нужное нам исключение, в самом верху файла нужно указать: 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10): # В конструктор BasePage добавим команду для неявного ожидания со значением по умолчанию в 10:
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
     
     def open(self):
-        self.browser.get(self.url)
+        self.browser.get(self.url) #Теперь добавим еще один метод open. Он должен открывать нужную страницу в браузере, используя метод get()
+    
+    # Теперь в этом же классе реализуем метод is_element_present, в котором будем перехватывать исключение. В него будем передавать два аргумента: как искать (css, id, xpath и тд) и собственно что искать (строку-селектор).     
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException): # except (имя исключения):
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,21 +1,14 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from .locators import MainPageLocators
+
+
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
-    def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
-    def __init__(self, browser, url, timeout=10):
-        self.browser = browser
-        self.url = url
-        self.browser.implicitly_wait(timeout)
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-        except (NoSuchElementException):
-            return False
-        return True
+    def should_be_login_link(self): 
+        # Теперь модифицируем метод проверки ссылки на логин так, чтобы он выдавал адекватное сообщение об ошибке:       
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    
     
